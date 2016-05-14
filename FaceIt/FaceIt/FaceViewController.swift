1fec6fb2eadf6f092e49c5eef15a592936a1de84
zhugejunwei <zhugerock@gmail.com>
2016-05-14 19:55:42 -0400

    split viewcontroller

--- FaceIt/FaceIt/FaceViewController.swift
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -57,14 +57,17 @@ class FaceViewController: UIViewController
     
     private var mouthCurvatures = [FacialExpression.Mouth.Frown: -1.0, .Grin:0.5, .Smile: 1.0, .Smirk:-0.5, .Neutral:0.0]
     private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed: 0.5,.Furrowed:-0.5,.Normal:0.0]
+    
     private func updateUI() {
-        switch expression.eyes {
-        case .Open: faceView.eyesOpen = true
-        case .Closed: faceView.eyesOpen = false
-        case .Squinting: faceView.eyesOpen = false
+        if faceView != nil {
+            switch expression.eyes {
+            case .Open: faceView.eyesOpen = true
+            case .Closed: faceView.eyesOpen = false
+            case .Squinting: faceView.eyesOpen = false
+            }
+            faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
+            faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
         }
-        faceView.mouthCurvature = mouthCurvatures[expression.mouth] ?? 0.0
-        faceView.eyeBrowTilt = eyeBrowTilts[expression.eyeBrows] ?? 0.0
     }
     
 }
